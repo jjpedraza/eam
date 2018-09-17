64ddd27b575d6fc7f21f0eaeae2d55c207882bda
printepolis@gmail.com <printepolis@gmail.com>
2018-09-17 01:14:32 -0500

    fijacion 17d

--- index.php
+++ index.php
@@ -17,7 +17,30 @@ echo "</lu>";
 
 
 
+echo "<div id='modular' class='ventana' style='width:90%; height:400px; overflow:auto;'><table class='tabla'><th>Lada</th><th>Reg</th>";
+$sql = "
+SELECT DISTINCT
+	SUBSTRING(celular, 1, 3 ) as Lada
+    -- ,(SELECT  GROUP_CONCAT(clave) FROM  ladas WHERE  clave like  CONCAT('%',Lada,'%'))	as Lugar
+    ,(select count(*) from celulares where celular like CONCAT('',Lada,'%')) as n
+	
+FROM
+	celulares 
 
+order by n desc
+";
+ 	$r2 = $conexion -> query($sql); while($f = $r2 -> fetch_array())
+    {
+        echo "<tr>";
+        echo "<td>".$f['Lada']."</td>";
+        //echo "<td><label style='font-size:7pt'>";
+        //echo str_replace(",", ", ", $f['Lugar']);
+        //echo "</label></td>";
+        echo "<td>".$f['n']."</td>";
+        
+        echo "</tr>";
+    }
+echo "</table></div>";
 
 
 include('src/footer.php');
