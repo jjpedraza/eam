39bfd34d94b525ea2fd05713d7a1104217359146
printepolis@gmail.com <printepolis@gmail.com>
2018-10-14 23:56:46 -0500

    x

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include('src/head.php');
 
-echo "<lu id ='menuprincipal'>";
+echo "<div class='ventana' ><lu id ='menuprincipal'>";
 
 echo "<li>";
 echo "<table><tr>";
@@ -39,65 +39,125 @@ echo "</li>";
 // echo "</tr></table>";
 // echo "</li>";
 
-echo "</lu>";
+echo "</lu></div>";
 
 
-echo "<div class='ventana' id='SMS_actividad'>";
-echo "<div>";
+echo "<div class='ventana' >";
 
+//Write table on result sms
+$sql = "
+SELECT DISTINCT(Brigada) AS QBrigada,
+(select count(*) from sms where Brigada=QBrigada) as Total,
+(select count(*) from sms where Brigada=QBrigada and estado='0') as Faltantes,
+(select count(*) from sms where Brigada=QBrigada and estado='1') as  Avance,
+(
+	CONCAT(
 
+		ROUND(
+		(
+			
+				(100 / (select count(*) from sms where Brigada=QBrigada) ) * 
+				(select count(*) from sms where Brigada=QBrigada and estado='1') 
+			
+		)
 
-// echo "<div id='modular' class='ventana' style='width:90%; height:400px; overflow:auto;'>";
-// echo "<div id='modular' class='ventana' style='width:40%; height:400px; overflow:auto;'><table class='tabla'><th>Lada</th><th>Reg</th>";
-// $sql = "
-// SELECT DISTINCT
-// 	SUBSTRING(celular, 1, 3 ) as Lada
-//     -- ,(SELECT  GROUP_CONCAT(clave) FROM  ladas WHERE  clave like  CONCAT('%',Lada,'%'))	as Lugar
-//     ,(select count(*) from celulares where celular like CONCAT('',Lada,'%')) as n
+	)
+	, '%')
 	
-// FROM
-// 	celulares 
+) as Porcentaje,
 
-// order by n desc
-// ";
-//  	$r2 = $conexion -> query($sql); while($f = $r2 -> fetch_array())
-//     {
-//         echo "<tr>";
-//         echo "<td>".$f['Lada']."</td>";
-//         //echo "<td><label style='font-size:7pt'>";
-//         //echo str_replace(",", ", ", $f['Lugar']);
-//         //echo "</label></td>";
-//         echo "<td>".$f['n']."</td>";
+(select sms.mensaje from sms where Brigada=QBrigada limit 1) as Mensaje,
+(select sms.fecha from sms where Brigada=QBrigada limit 1) as Fecha,
+(select sms.envia from sms where Brigada=QBrigada limit 1) as Admin
+
+
+
+
+FROM sms
+
+
+ORDER BY  Porcentaje DESC
+
+-- Porcentaje = (total -avance)/total * 100
+";
+	$StOK='background-color:auto; color:#A2C30D;cursor:pointer;';
+	$St='background-color:auto; color:auto; font-size:14pt;cursor:pointer;';
+
+    echo "<table class='tabla'>";
+    echo "<th>Brigada</th><th>Avance</th>";
+    $r2 = $conexion -> query($sql); while($f = $r2 -> fetch_array())
+    {
+    	if ($f['Porcentaje']=='100%'){
+    		echo "<tr style='$StOK'>";
+    				echo "<td style='$StOK' title='".$f['Fecha']." | ".$f['Mensaje'].", Creada por ".$f['Admin']."'>".$f['QBrigada']."</td>";
+    				echo "<td style='$StOK' title='Total de mensajes: ".$f['Total']."'>".$f['Porcentaje']."</td>";
+		    echo "</tr>";
+    	} else {
+			echo "<tr style='$St'>";
+    				echo "<td style='$St' title='".$f['Fecha']." | ".$f['Mensaje'].", Creada por ".$f['Admin']."'>".$f['QBrigada']."</td>";
+    				echo "<td style='$St' title='Total de mensajes: ".$f['Total']."(Sin entregar: ".$f['Faltantes'].", Entregados: ".$f['Avance'].")'>".$f['Porcentaje']."</td>";
+		    echo "</tr>";
+    	}
+    	
+
+    }
+    echo "</table>";
+
+echo "<div><hr>";
+
+
+
+// echo "<div id='modular2' class='ventana' >";
+echo "<div  class='ventana' ><table class='tabla'><th>Lada</th><th>Reg</th>";
+$sql = "
+SELECT DISTINCT
+	SUBSTRING(celular, 1, 3 ) as Lada
+    -- ,(SELECT  GROUP_CONCAT(clave) FROM  ladas WHERE  clave like  CONCAT('%',Lada,'%'))	as Lugar
+    ,(select count(*) from celulares where celular like CONCAT('',Lada,'%')) as n
+	
+FROM
+	celulares 
+
+order by n desc
+";
+ 	$r2 = $conexion -> query($sql); while($f = $r2 -> fetch_array())
+    {
+        echo "<tr>";
+        echo "<td>".$f['Lada']."</td>";
+        //echo "<td><label style='font-size:7pt'>";
+        //echo str_replace(",", ", ", $f['Lugar']);
+        //echo "</label></td>";
+        echo "<td>".$f['n']."</td>";
         
-//         echo "</tr>";
-//     }
-// echo "</table></div>";
+        echo "</tr>";
+    }
+echo "</table></div>";
 
 
-// echo "<div id='modular' class='ventana' style='width:40%; height:400px; overflow:auto;'><table class='tabla'><th>Lada</th><th>Rama</th>";
+echo "<div  class='ventana' ><table class='tabla'><th>Lada</th><th>Rama</th>";
 
-// $sql = "
-// select  DISTINCT SUBSTRING(celular, 1, 3 ) as Lada,
+$sql = "
+select  DISTINCT SUBSTRING(celular, 1, 3 ) as Lada,
 
-// SUBSTRING(celular, 4, 3 ) as Rama
+SUBSTRING(celular, 4, 3 ) as Rama
 
 
 
-// from celulares
+from celulares
 
-// ";
-//  	$r2 = $conexion -> query($sql); while($f = $r2 -> fetch_array())
-//     {
-//         echo "<tr>";
-//         echo "<td>".$f['Lada']."</td>";
-//         //echo "<td><label style='font-size:7pt'>";
-//         //echo str_replace(",", ", ", $f['Lugar']);
-//         //echo "</label></td>";
-//         echo "<td>".$f['Rama']."</td>";
+";
+ 	$r2 = $conexion -> query($sql); while($f = $r2 -> fetch_array())
+    {
+        echo "<tr>";
+        echo "<td>".$f['Lada']."</td>";
+        //echo "<td><label style='font-size:7pt'>";
+        //echo str_replace(",", ", ", $f['Lugar']);
+        //echo "</label></td>";
+        echo "<td>".$f['Rama']."</td>";
         
-//         echo "</tr>";
-//     }
-// echo "</table></div> </div>";
+        echo "</tr>";
+    }
+echo "</table></div> ";
 
 
 
@@ -108,11 +168,11 @@ include('src/footer.php');
 ?>
 
 <script >
-function CargaAvancesSMS(){
-        $.get("sms_actividad.php", {}, function(htmlexterno){$("#SMS_actividad").html(htmlexterno);});
-        console.log("Consultando CargaAvancesSMS");
+// function CargaAvancesSMS(){
+//         $.get("sms_actividad.php", {}, function(htmlexterno){$("#SMS_actividad").html(htmlexterno);});
+//         console.log("Consultando CargaAvancesSMS");
  
-}
+// }
 
 
 // function CargaDispositivos(){
@@ -122,7 +182,7 @@ function CargaAvancesSMS(){
 // }
  
 
-setInterval(CargaAvancesSMS,1000);
+setInterval(CargaAvancesSMS,5000);
 // setInterval(CargaDispositivos,4000);
 
 
